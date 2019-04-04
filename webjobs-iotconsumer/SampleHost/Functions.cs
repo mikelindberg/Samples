9027ed9885f30e9ef237d196b1b3dd25d6de7291
Zoran Jovanovic <zojovano@microsoft.com>
2019-04-04 19:02:01 +0200

    adding webjob consumer code

---
+++ webjobs-iotconsumer/SampleHost/Functions.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using SampleHost.Filters;
+
+namespace SampleHost
+{
+    [ErrorHandler]
+    public class Functions
+    {
+
+        public void ProcessEvents([EventHubTrigger("csucsa-demo-iot-a", Connection = "TestEventHubConnection")] EventData[] events,
+            ILogger log)
+        {
+            foreach (var evt in events)
+            {
+                log.LogInformation($"Event processed (Offset={evt.SystemProperties.Offset}, SequenceNumber={evt.SystemProperties.SequenceNumber})");
+            }
+        }
+    }
+}
---
+++ webjobs-iotconsumer/SampleHost/Program.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace SampleHost
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = new HostBuilder()
+                .UseEnvironment("Development")
+                .ConfigureWebJobs(b =>
+                {
+                    b.AddAzureStorageCoreServices()
+                    .AddEventHubs();
+                })
+                .ConfigureAppConfiguration(b =>
+                {
+                    b.AddJsonFile("appsettings.json");
+                })
+                .ConfigureLogging((context, b) =>
+                {
+                    b.SetMinimumLevel(LogLevel.Debug);
+                    b.AddConsole();
+
+                    // If this key exists in any config, use it to enable App Insights
+                    string appInsightsKey = context.Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"];
+                    if (!string.IsNullOrEmpty(appInsightsKey))
+                    {
+                        b.AddApplicationInsights(o => o.InstrumentationKey = appInsightsKey);
+                    }
+                })
+                .UseConsoleLifetime();
+
+            var host = builder.Build();
+            using (host)
+            {
+                await host.RunAsync();
+            }
+        }
+    }
+}
