2a10801167c9dfa3ecbe4df8ab6d73e3b8b5d87f
Katriendg <my@myemail.com>
2019-04-19 14:48:46 +0200

    device type filter and simulator

--- AKS_IoTHub_Reader/telemetryReader/DocumentService.cs
+++ AKS_IoTHub_Reader/telemetryReader/DocumentService.cs
@@ -3,7 +3,12 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.Documents;
 using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Spatial;
 using Microsoft.Azure.EventHubs;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using telemetryReader.Models;
+
 
 namespace telemetryReader
 {
@@ -18,10 +23,12 @@ namespace telemetryReader
             this.dbName = dbName;
             this.collectionName = collectionName;
             _documentClient = new DocumentClient(new Uri(endpoint), key,
-                    new ConnectionPolicy{
+                    new ConnectionPolicy
+                    {
                         ConnectionMode = ConnectionMode.Direct,
-                        RequestTimeout = new TimeSpan(1, 0, 0),
-                        RetryOptions = new RetryOptions{
+                        RequestTimeout = new TimeSpan(0, 30, 0),
+                        RetryOptions = new RetryOptions
+                        {
                             MaxRetryAttemptsOnThrottledRequests = 10,
                             MaxRetryWaitTimeInSeconds = 30
                         }
@@ -34,36 +41,52 @@ namespace telemetryReader
         //Store the document in a Cosmos DB collection
         public async Task StoreDocument(EventData eventData)
         {
-            Console.WriteLine("Storing Document...");
-            var payload = Encoding.UTF8.GetString(eventData.Body.Array, eventData.Body.Offset, eventData.Body.Count);
             
-            var newDocument = new
-            {
-                processedUtcTime = DateTime.UtcNow,
-                enqueudUtcTime = eventData.SystemProperties.EnqueuedTimeUtc,
-                partition = eventData.SystemProperties.PartitionKey,
-                messageOffset = eventData.Body.Offset,
-                deviceid = eventData.SystemProperties["iothub-connection-device-id"].ToString(),
-                payload = payload
-            };
+            var payload = Encoding.UTF8.GetString(eventData.Body.Array, eventData.Body.Offset, eventData.Body.Count);
 
-            try{
-                await _documentClient.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri(dbName, collectionName), newDocument);
-            }
-            catch(Exception e)
+            //parse the data and insert depending on message schema type / if it's not a truck we discard the message
+            TruckDeviceEvent parsed = JsonConvert.DeserializeObject<TruckDeviceEvent>(payload);
+
+            Console.WriteLine("EventData contains key ..." + eventData.Properties.ContainsKey("$$MessageSchema").ToString());
+            
+            if (eventData.Properties.ContainsKey("$$MessageSchema") && eventData.Properties["$$MessageSchema"].ToString().ToLower() == "truck-sensors;v1")
             {
-                if(e is DocumentClientException docEx)
+                Console.WriteLine("Parsing Truck Document...");
+
+                var newDocument = new
                 {
-                    Console.WriteLine($"ERROR with document creation - Statuc code {docEx.StatusCode} - {docEx.Message}");
-                }else
+                    //id = eventData.SystemProperties["iothub-connection-device-id"].ToString(),
+                    processedUtcTime = DateTime.UtcNow,
+                    enqueudUtcTime = eventData.SystemProperties.EnqueuedTimeUtc,
+                    partition = eventData.SystemProperties.PartitionKey,
+                    messageOffset = eventData.Body.Offset,
+                    deviceid = eventData.SystemProperties["iothub-connection-device-id"].ToString(),
+                    devicetype = "truck",
+                    location = new Point(parsed.longitude, parsed.latitude),
+                    temperature = parsed.temperature
+                };
+
+                try
                 {
-                    Console.WriteLine($"ERROR CosmosDB - {e.Message}");
+                    Console.WriteLine("Inserting new doc...");
+                    //Upsert instead?
+                    await _documentClient.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri(dbName, collectionName), newDocument);
+                }
+                catch (Exception e)
+                {
+                    if (e is DocumentClientException docEx)
+                    {
+                        Console.WriteLine($"ERROR with document creation - Statuc code {docEx.StatusCode} - {docEx.Message}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ERROR CosmosDB - {e.Message}");
+                    }
                 }
             }
 
 
 
-            
         }
     }
 }
